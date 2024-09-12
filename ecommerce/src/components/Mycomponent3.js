07bf0bc8afbe6354629322cdd375e512658ce764
AbhiSTDW <abhishek10ygr@gmail.com>
2024-09-12 08:15:19 +0530

    commit

---
+++ ecommerce/src/components/Mycomponent3.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const Mycomponent3 = () => {
+  return (
+    <div>
+    <table border={1}>
+      <thead>
+        <tr>
+          <th>Roll No</th>
+          <th>name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Abhi</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Rahul</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  )
+}
